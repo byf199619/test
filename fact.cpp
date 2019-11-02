02359c6264598430c6d249252a2d9cc5ca96ddc1
byf199619 <50740615+byf199619@users.noreply.github.com>
2019-11-02 10:14:54 +0800

    fact

---
+++ fact.cpp
@@ -0,0 +1,12 @@
+
+
+
+int fact(int val)
+{
+	int ret = 1;
+	while(val > 1)
+	{
+	ret *= val--;
+	}
+	return ret;
+}
---
+++ main.cpp
@@ -0,0 +1,12 @@
+
+#include<iostream>
+#include "fact.cpp"
+using namespace std;
+
+
+int main()
+{
+	int j = fact(5);
+	cout << "5! is "<< j << endl;
+	return 0;
+}
